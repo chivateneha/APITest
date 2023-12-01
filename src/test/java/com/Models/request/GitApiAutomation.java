43e3c4f7d2db6d8d0d8c997e3dec7dcb992c2671
chivateneha <chivateneha@gmail.com>
2023-12-01 16:10:08 -0600

    Updated Code- working!

--- src/test/java/com/Models/request/GitApiAutomation.java
+++ src/test/java/com/Models/request/GitApiAutomation.java
@@ -1,8 +1,10 @@
 package com.Models.request;
 
+import java.io.File;
+
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
+import io.restassured.*;
 import com.Models.response.CreateRepoPOJO;
 import com.Models.response.UpdatePOJO;
 
@@ -10,11 +12,12 @@ import com.Models.response.UpdatePOJO;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.http.Header;
+import io.restassured.module.jsv.JsonSchemaValidator;
 import io.restassured.response.Response;
 
 public class GitApiAutomation {
-private static String mytoken="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";
-	
+
+private static String mytoken2="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";
 	
 	@BeforeTest
 	public void setUp() {
@@ -23,10 +26,12 @@ private static String mytoken="github_pat_11BDQ27RA06CyE6BQD7VW2_pBW86VpapqWOOPO
 	
 	@Test
 	public void GetSingleRepoPOJO() {
-		Header header=new Header("token",mytoken);
+		Header header=new Header("token","mytoken2");
+		
 		Response res=RestAssured
 		.given()
-		.header(header)
+		//.header(header)
+		.header("Authorization", "Bearer " + mytoken2)
 		.when()
 		.get("/repos/chivateneha/SeleniuTestNG");
 		res.prettyPrint();
@@ -40,10 +45,10 @@ private static String mytoken="github_pat_11BDQ27RA06CyE6BQD7VW2_pBW86VpapqWOOPO
 	
      @Test	
      public void GetSingleNonExistRepo(){
-	 Header header=new Header("token",mytoken);
+	 Header header=new Header("token","mytoken2");
 		Response res=RestAssured
 		.given()
-		.header(header)
+		.header("Authorization", "Bearer " + mytoken2)
 		.when()
 		.get("/repos/chivateneha/Selenium");
 		res.prettyPrint();
@@ -56,10 +61,10 @@ private static String mytoken="github_pat_11BDQ27RA06CyE6BQD7VW2_pBW86VpapqWOOPO
  }
    @Test
     public void GetAllRepos() {
-    	Header header =new Header("token",mytoken);
+    	Header header =new Header("token","mytoken2");
     	Response res= RestAssured
     			.given()
-    			.header(header)
+    			.header("Authorization", "Bearer " + mytoken2)
     			.when()
     			.get("user/repos");
     			res.prettyPrint();
@@ -68,21 +73,26 @@ private static String mytoken="github_pat_11BDQ27RA06CyE6BQD7VW2_pBW86VpapqWOOPO
     			.then()
     			.statusCode(200)
     			.contentType(ContentType.JSON);
+    			res.then()
+    			 .assertThat().
+    			    body(JsonSchemaValidator.matchesJsonSchema(new File ("C:\\Users\\Chiva\\java2023\\APITestFramework\\src\\test\\resources\\AllUserSchema.json")));
+
+  
     			res.prettyPrint();
-	
+	            
 }
    @Test
   public void CreateNewRepo() {
-		Header header = new Header("token",mytoken);
+		Header header = new Header("token","mytoken2");
 		CreateRepoPOJO data= new CreateRepoPOJO();
-		data.setName("Hello-World");
+		data.setName("hello");
 		data.setDescription("This is your first repo!");
 		data.setHomepage("https://api.github.com/");
 		data.setPrivate("false");
 		
 		Response res=RestAssured
 				.given()
-				.header(header)
+				.header("Authorization", "Bearer " + mytoken2)
 				.contentType(ContentType.JSON)
 				.body(data)
 				.when()
@@ -91,10 +101,12 @@ private static String mytoken="github_pat_11BDQ27RA06CyE6BQD7VW2_pBW86VpapqWOOPO
 		.then()
 		.statusCode(201)
 		.contentType(ContentType.JSON);
+		res.prettyPrint();
+		
   }
-   
+   @Test
    public void CreateExistingRepo() {
-	   Header header = new Header("token",mytoken);
+	   Header header = new Header("token","mytoken");
 		CreateRepoPOJO data= new CreateRepoPOJO();
 		data.setName("Hello-World");
 		data.setDescription("This is your first repo!");
@@ -103,7 +115,7 @@ private static String mytoken="github_pat_11BDQ27RA06CyE6BQD7VW2_pBW86VpapqWOOPO
 		
 		Response res=RestAssured
 				.given()
-				.header(header)
+				.header("Authorization", "Bearer " + mytoken2)
 				.contentType(ContentType.JSON)
 				.body(data)
 				.when()
@@ -112,68 +124,69 @@ private static String mytoken="github_pat_11BDQ27RA06CyE6BQD7VW2_pBW86VpapqWOOPO
 		.then()
 		.statusCode(201)
 		.contentType(ContentType.JSON);
-	   
+		res.prettyPrint();
 	   
    }
    
    @Test
    public void UpdateRepo() {
-		Header header = new Header("token",mytoken);
+		Header header = new Header("token","mytoken2");
 		UpdatePOJO data= new UpdatePOJO();
-		data.setName("byebye-World1");
+		data.setName("hello");
 		data.setDescription("my repository created using apis after update");
 		data.setPrivate("false");
 		Response res=RestAssured
 				.given()
-				.header(header)
+				.header("Authorization", "Bearer " + mytoken2)
 				.contentType(ContentType.JSON)
 				.body(data)
 				.when()
-				.patch("user/repos/chivateneha/Hello-World2");	
+				.patch("/repos/chivateneha/byebye1");	
 		
 		res
 		.then()
 		.statusCode(200)
 		.contentType(ContentType.JSON);
-		
+		res.prettyPrint();
 	}
    
    @Test
    public void DeleteRepo() {
-		Header header = new Header("token",mytoken);
+
+		Header header = new Header("token","mytoken2");
 		//UpdatePOJO data = new UpdatePOJO();
 		Response res=RestAssured
 				.given()
-				.header(header)
-				.contentType(ContentType.JSON)
+				.header("Authorization", "Bearer " + mytoken2)
+				//.contentType(ContentType.JSON)
 				.when()
-		        .delete("/repos/chivateneha/Hello-World2");	
+		        .delete("/repos/chivateneha/hello");	
 		
 		res
 		.then()
-		.statusCode(200)
-		.contentType(ContentType.JSON);
+		.statusCode(204);
+		//.contentType(ContentType.JSON);
 		
-		System.out.println(res.body());
+		System.out.println(res.statusCode());
 		
 	}
    @Test
    public void DeleteNonExist() {
-	   Header header = new Header("token",mytoken);
+	   Header header = new Header("token","mytoken2");
 		UpdatePOJO data = new UpdatePOJO();
 		Response res=RestAssured
 				.given()
-				.header(header)
-				.contentType(ContentType.JSON)
+				.header("Authorization", "Bearer " + mytoken2)
+				//.contentType(ContentType.JSON)
 				.when()
-		        .delete("/repos/chivateneha/byebye1");	
+		        .delete("/repos/chivateneha/hello");	
 		
 		res
 		.then()
-		.statusCode(404)
-		.contentType(ContentType.JSON);
+		.statusCode(404);
+		//.contentType(ContentType.JSON);
 		
-		System.out.println(res.body());
+		System.out.println(res.statusCode());
 		
 	   
    }
